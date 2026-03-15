ed9654ca953684e4b78ad3e80df09838434040d2
shcommit <happysh_s2@naver.com>
2026-03-15 23:24:14 +0900

    SSR 적용 완료 홈화면, 검색화면

---
+++ section02_page_router/src/lib/fetch-oneBook.ts
@@ -0,0 +1,15 @@
+import { Book } from "@/types";
+
+export default async function fetchOneBook(id: number): Promise<Book | null> {
+  const url = `http://localhost:12345/book/${id}`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error();
+
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+    return null;
+  }
+}
---
+++ section02_page_router/src/lib/fetch-randomBooks.ts
@@ -0,0 +1,13 @@
+import { Book } from "@/types";
+export default async function fetchRandomBooks(): Promise<Book[]> {
+  const url = `http://localhost:12345/book/random`;
+
+  try {
+    const response = await fetch(url);
+    if (!response.ok) throw new Error();
+    return await response.json();
+  } catch (err) {
+    console.log(err);
+    return [];
+  }
+}
