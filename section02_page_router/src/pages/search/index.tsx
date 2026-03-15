f6c5617e9e44bb2587cf26ee5f1f9fa44070ff67
shcommit <happysh_s2@naver.com>
2026-03-16 00:30:24 +0900

    빌드 시점에 데이터 요청후, 결과물로 html을만들어두는 SSG!

--- section02_page_router/src/pages/search/index.tsx
+++ section02_page_router/src/pages/search/index.tsx
@@ -1,25 +1,24 @@
-import { ReactNode } from "react";
+import { ReactNode, useState, useEffect } from "react";
 import SearchableLayout from "@/components/searchable-layout";
 import BookItem from "@/components/book-item";
-import { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { Book } from "@/types";
 import fetchBooks from "@/lib/fetch-books";
+import { useRouter } from "next/router";
 
-/// context라는 매개변수에는 현재 브라우저로부터 받은 요청에 대한 모든 정보가 다 포함되어 있다.
-export const getServerSideProps = async (
-  context: GetServerSidePropsContext,
-) => {
-  const q = context.query.q;
-  const books = await fetchBooks(q as string);
-  return {
-    props: {
-      books,
-    },
+export default function Page() {
+  const [books, setBooks] = useState<Book[]>([]);
+  const router = useRouter();
+  const q = router.query.q;
+  const fetchSearchResult = async () => {
+    const data = await fetchBooks(q as string);
+    setBooks(data);
   };
-};
+  useEffect(() => {
+    if (q) {
+      fetchSearchResult();
+    }
+  }, [q]);
 
-export default function Page(
-  {books}: InferGetServerSidePropsType<typeof getServerSideProps>,
-) {
   return (
     <div>
       {books.map((book) => (
