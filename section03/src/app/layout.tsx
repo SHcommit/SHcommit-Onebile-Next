24348b447b6b31bbad9ec70bdd41a46bca46e8cb
shcommit <happysh_s2@naver.com>
2026-03-16 23:17:09 +0900

    section3 앱라우터 풀라우팅 적용기

--- section03/src/app/layout.tsx
+++ section03/src/app/layout.tsx
@@ -1,6 +1,29 @@
 import "./globals.css";
 import Link from "next/link";
 import style from "./layout.module.css";
+import { BookData } from "@/types";
+
+/// Footer에서도 book 전체 종류를 받아오고
+/// Search 페이지에서도 book 전체 종류를 받아온다.
+/// 요청 url이 같은데 서로 다른 컴포넌트에서 요청을 한다..
+/// 상당히 비효율적인건데
+/// Next에서는 Request memoization을 지원해주기 때문에 괜찮다.
+/// 각각의 렌더링될 때 호출했던 url의 response를 캐싱해주기 때문임 : )
+async function Footer() {
+  const response = await fetch(
+    `${process.env.NEXT_PUBLIC_API_SERVER_URL}/book`,
+  );
+  if (!response.ok) return <footer>제작 @Seung-Hyun, Yang</footer>;
+  const books: BookData[] = await response.json();
+  const numberOfBooks = books.length;
+
+  return (
+    <footer>
+      <div>제작 @Seung-Hyun, Yang</div>
+      <div>{numberOfBooks}개의 도서가 등록되어 있습니다.</div>
+    </footer>
+  );
+}
 
 export default function RootLayout({
   children,
@@ -15,7 +38,7 @@ export default function RootLayout({
             <Link href={"/"}>📚 ONEBITE BOOKS</Link>
           </header>
           <main>{children}</main>
-          <footer>제작 @winterlood</footer>
+          <Footer />
         </div>
       </body>
     </html>
