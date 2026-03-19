d7f4cdd4257b4ace3e3a4e1bd4837812dc2af759
shcommit <happysh_s2@naver.com>
2026-03-19 20:17:17 +0900

    add: section07 serverAction. ReviewEditor에서 작성하기 눌렀을때 form 비활성화 로직추가

--- section07_serverAction/src/components/ReviewEditor.tsx
+++ section07_serverAction/src/components/ReviewEditor.tsx
@@ -1,16 +1,37 @@
+"use client";
+
 import style from "./ReviewEditor.module.css";
 import { createReviewAction } from "@/actions/createReview.action";
+import { useActionState } from "react";
 
 export default function ReviewEditor({ bookId }: { bookId: string }) {
+  const [state, formAction, isPending] = useActionState(
+    createReviewAction,
+    null,
+  );
+
   return (
     <section>
-      <form className={style.formContainer} action={createReviewAction}>
+      <form className={style.formContainer} action={formAction}>
         {/** 인풋에서 북 아이디는 안보이게! hidden + readOnly */}
         <input name="bookId" value={bookId} hidden readOnly />
-        <textarea required name="content" placeholder="리뷰 내용" />
+        <textarea
+          disabled={isPending}
+          required
+          name="content"
+          placeholder="리뷰 내용"
+        />
         <div className={style.submitContainer}>
-          <input required name="author" placeholder="작성자" />
-          <button type="submit"> 작성하기</button>
+          <input
+            disabled={isPending}
+            required
+            name="author"
+            placeholder="작성자"
+          />
+          <button disabled={isPending} type="submit">
+            {" "}
+            {isPending ? "..." : "작성하기"}
+          </button>
         </div>
       </form>
     </section>
